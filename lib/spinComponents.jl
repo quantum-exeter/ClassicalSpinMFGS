24836c14c37d52a93777e84f570ac80995ab1165
Charlie <charlotte.hogg@me.com>
2022-07-03 23:38:14 +0100

    Structs added to file from types.jl.

--- lib/spinComponents.jl
+++ lib/spinComponents.jl
@@ -8,4 +8,10 @@ sy(θ, ϕ) = sin(θ)*sin(ϕ)
 sz(θ) = cos(θ)
 
 ### Spin Coupling Operators ###
+
+struct CouplingAngles
+    θ
+    ϕ
+end
+
 sc(θ, ϕ, ang::CouplingAngles) = sx(θ, ϕ)*sin(ang.θ)*cos(ang.ϕ) + sy(θ, ϕ)*sin(ang.θ)*cos(ang.ϕ) + sz(θ)*cos(ang.θ)
