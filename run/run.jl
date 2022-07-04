0b7a56562cb9c24fab467abca28dcd90d411f135
Charlie Hogg <charlotte.hogg@me.com>
2022-07-04 14:40:38 +0100

    Magnetisation function name changed.

--- run/run.jl
+++ run/run.jl
@@ -4,14 +4,14 @@ using DataFrames
 include("../lib/MFGS.jl")
 using .MFGS
 
-prm = LorPrm1D(7, 5, 10)
+prm = LorPrm1D(2, 5, 10)
 ang =  CouplAng1D(π/2, 0.0)
 
 ### Temperature Range ###
 T = exp10.(range(-2, 3, length=100));
 
 ### z-Magnetisations ###
-magz_list = [mag(i, ang, prm) for i in T]
+magz_list = [magz(i, ang, prm) for i in T]
 
 ### Store Values ###
 df = DataFrame(hcat(T, magz_list), :auto)
