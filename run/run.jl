de758747c8e352336c34dbd2ba53c1758f93b6e6
Charlie Hogg <charlotte.hogg@me.com>
2022-07-04 14:10:33 +0100

    Functions added to export magnetisation values.

--- run/run.jl
+++ run/run.jl
@@ -1,7 +1,18 @@
+using CSV
+using DataFrames
+
 include("../lib/MFGS.jl")
 using .MFGS
 
 prm = LorPrm1D(7, 5, 10)
 ang =  CouplAng1D(π/2, 0.0)
 
-mag(1.0, ang, prm)
+### Temperature Range ###
+T = exp10.(range(-2, 3, length=100));
+
+### z-Magnetisations ###
+magz_list = [mag(i, ang, prm) for i in T]
+
+### Store Values ###
+df = DataFrame(hcat(T, magz_list), :auto)
+CSV.write("C://Users//crh222//Data//Classical//test.csv",  df, header = ["T", "sz"]);
