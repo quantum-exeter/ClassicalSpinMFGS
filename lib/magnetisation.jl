7c6dd31eda82963ece1f0f5700f907d8892ca3e0
Charlie Hogg <charlotte.hogg@me.com>
2022-07-04 15:13:17 +0100

    BigFloat() added to exponential.

--- lib/magnetisation.jl
+++ lib/magnetisation.jl
@@ -5,6 +5,6 @@
 function magz(T, ang::CouplingAngles, prm::Lorentzian)
   𝒵 = 𝒵s(T, ang, prm)
   H(θ, ϕ) = Heff(θ, ϕ, ang, prm)
-  I(θ, ϕ) = sin(θ)*cos(θ)*(1/𝒵)*exp(-(1/T)*H(θ, ϕ))
+  I(θ, ϕ) = sin(θ)*cos(θ)*(1/𝒵)*exp(-BigFloat((1/T)*H(θ, ϕ)))
   return dblquadgk(I, [0.0, 0.0], [π, 2π])
 end
