764806c697662c3ed5e329bab55d8a6c2a8494f8
Charlie Hogg <charlotte.hogg@me.com>
2022-07-04 11:56:11 +0100

    Effective hamiltonian function overloaded.

--- lib/hamiltonians.jl
+++ lib/hamiltonians.jl
@@ -3,9 +3,9 @@
 ###########################
 
 ### Reorganisation Energy ###
-Q(prm::LorPrm) = (prm.α*ωL)/(2*prm.ω0^2)
+Q(prm::LorPrm) = (prm.α)/(2*prm.ω0^2)
 
 # Effective Hamiltonians ##
-Heff1D(θ, ϕ, ang1::CouplingAngles, prm1::LorPrm) = -sign(γ)*sz(θ) - Q(prm1)*sc(θ, ϕ, ang1)^2
-Heff2D(θ, ϕ, ang1::CouplingAngles, ang2::CouplingAngles, prm1::LorPrm, prm2::LorPrm) = -sign(γ)*sz(θ) - Q(prm1)*sc(θ, ϕ, ang1)^2 - Q(prm2)*sc(θ, ϕ, ang2)^2
-Heff3D(θ, ϕ, ang1::CouplingAngles, ang2::CouplingAngles, ang3::CouplingAngles, prm1::LorPrm, prm2::LorPrm, prm3::LorPrm) = -sign(γ)*sz(θ) - Q(prm1)*sc(θ, ϕ, ang1)^2 - Q(prm2)*sc(θ, ϕ, ang2)^2 - Q(prm3)*sc(θ, ϕ, ang3)^2
+Heff(θ, ϕ, ang1::CouplingAngles, prm1::LorPrm) = -sign(γ)*sz(θ) - Q(prm1)*sc(θ, ϕ, ang1)^2
+Heff(θ, ϕ, ang1::CouplingAngles, prm1::LorPrm, ang2::CouplingAngles, prm2::LorPrm) = -sign(γ)*sz(θ) - Q(prm1)*sc(θ, ϕ, ang1)^2 - Q(prm2)*sc(θ, ϕ, ang2)^2
+Heff(θ, ϕ, ang1::CouplingAngles, prm1::LorPrm, ang2::CouplingAngles, prm2::LorPrm, ang3::CouplingAngles, prm3::LorPrm) = -sign(γ)*sz(θ) - Q(prm1)*sc(θ, ϕ, ang1)^2 - Q(prm2)*sc(θ, ϕ, ang2)^2 - Q(prm3)*sc(θ, ϕ, ang3)^2
