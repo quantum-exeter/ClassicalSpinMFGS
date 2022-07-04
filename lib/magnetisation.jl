bd604406ce1a890c440312e0d8889b26482d2b93
Charlie Hogg <charlotte.hogg@me.com>
2022-07-04 14:05:22 +0100

    Magnetisation function renamed.

--- lib/magnetisation.jl
+++ lib/magnetisation.jl
@@ -2,7 +2,7 @@
 #### magnetisation.jl ####
 ##########################
 
-function mag(T, ang::CouplingAngles, prm::Lorentzian)
+function magz(T, ang::CouplingAngles, prm::Lorentzian)
   𝒵 = 𝒵s(T, ang, prm)
   H(θ, ϕ) = Heff(θ, ϕ, ang, prm)
   I(θ, ϕ) = sin(θ)*cos(θ)*(1/𝒵)*exp(-(1/T)*H(θ, ϕ))
