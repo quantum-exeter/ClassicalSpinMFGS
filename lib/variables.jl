7930121e2e353a9efb8ea3ed8c90cfdfa032aac9
Charlie Hogg <charlotte.hogg@me.com>
2022-07-04 13:50:46 +0100

    Sign of gyromagnetic ratio changed to match Paper 1.

--- lib/variables.jl
+++ lib/variables.jl
@@ -2,7 +2,7 @@
 #### variables.jl ####
 ######################
 
-γ = -1 # Gyronmagnetic ratio
+γ = 1 # Gyronmagnetic ratio
 Bext = 10 # External magnetic field
 ωL = abs(γ)*Bext # Larmor frequency
 s0 = 1/2 # Spin size
