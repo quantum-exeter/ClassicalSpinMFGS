0d85cdc98dd4c1ee66dfc3ab4fced9ef596521f1
Charlie Hogg <charlotte.hogg@me.com>
2022-10-05 14:08:48 +0100

    sx and sy magnetisations added.

--- lib/magnetisation.jl
+++ lib/magnetisation.jl
@@ -2,9 +2,23 @@
 #### magnetisation.jl ####
 ##########################
 
+function magx(T, ang::CouplingAngles, prm::Lorentzian)
+  𝒵 = 𝒵s(T, ang, prm)
+  H(θ, ϕ) = Heff(θ, ϕ, ang, prm)
+  I(θ, ϕ) = sx(θ, ϕ)*(1/𝒵)*exp(-(1/T)*H(θ, ϕ))*sin(θ)
+  return dblquadgk(I, [0.0, 0.0], [π, 2π])
+end
+
+function magy(T, ang::CouplingAngles, prm::Lorentzian)
+  𝒵 = 𝒵s(T, ang, prm)
+  H(θ, ϕ) = Heff(θ, ϕ, ang, prm)
+  I(θ, ϕ) = sy(θ, ϕ)*(1/𝒵)*exp(-(1/T)*H(θ, ϕ))*sin(θ)
+  return dblquadgk(I, [0.0, 0.0], [π, 2π])
+end
+
 function magz(T, ang::CouplingAngles, prm::Lorentzian)
   𝒵 = 𝒵s(T, ang, prm)
   H(θ, ϕ) = Heff(θ, ϕ, ang, prm)
-  I(θ, ϕ) = sin(θ)*cos(θ)*(1/𝒵)*exp(-BigFloat((1/T)*H(θ, ϕ)))
+  I(θ, ϕ) = sz(θ)*(1/𝒵)*exp(-(1/T)*H(θ, ϕ))*sin(θ)
   return dblquadgk(I, [0.0, 0.0], [π, 2π])
 end
