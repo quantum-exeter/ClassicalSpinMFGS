d3c4bcb64b37fece1a4eb1858d1221dda2f2f0e9
Charlie Hogg <charlotte.hogg@me.com>
2022-10-05 14:10:56 +0100

    sx and sy magnetisations added.

--- run/run.jl
+++ run/run.jl
@@ -4,15 +4,24 @@ using DataFrames
 include("../lib/MFGS.jl")
 using .MFGS
 
-prm = LorPrm1D(2, 5, 10)
-ang =  CouplAng1D(π/2, 0.0)
+# prma = 2.0, 0.001, 10.0
+# prmb = 2.0, 0.001, 1.0
+
+# prm = LorPrm1D(2.0, 0.001, 10.0) 
+# prm = LorPrm2D(2.0, 0.001, 10.0, 2.0, 0.001, 10.0) 
+prm = LorPrm3D(2.0, 0.001, 10.0, 2.0, 0.001, 10.0, 2.0, 0.001, 10.0) # Lorentzian parameters
+# ang =  CouplAng1D(0.0, 0.0) # Coupling angles
+# ang =  CouplAng2D(π/2, 0.0, 0.0, 0.0) # Coupling angles
+ang =  CouplAng3D(π/2, 0.0, π/2, π/2, 0.0, 0.0) # Coupling angles
 
 ### Temperature Range ###
-T = exp10.(range(-2, 3, length=100));
+T = exp10.(range(-2, 3, length=2));
 
 ### z-Magnetisations ###
+magx_list = [magx(i, ang, prm) for i in T]
+magy_list = [magy(i, ang, prm) for i in T]
 magz_list = [magz(i, ang, prm) for i in T]
 
 ### Store Values ###
-df = DataFrame(hcat(T, magz_list), :auto)
-CSV.write("C://Users//crh222//Data//Classical//test.csv",  df, header = ["T", "sz"]);
+df = DataFrame(hcat(T, magz_list, magy_list, magz_list), :auto)
+CSV.write("C://Users//crh222//Data//RC//Classical//MFGS_3D_prma.csv",  df, header = ["T", "sx", "sy", "sz"])
