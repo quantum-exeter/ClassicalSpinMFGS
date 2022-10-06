df8bece81058db2344425997e364ccf48d1ce566
Charlie Hogg <charlotte.hogg@me.com>
2022-10-06 11:44:11 +0100

    Tolerances added to sx and sy functions to allow for convergence.

--- lib/magnetisation.jl
+++ lib/magnetisation.jl
@@ -6,14 +6,14 @@ function magx(T, ang::CouplingAngles, prm::Lorentzian)
   𝒵 = 𝒵s(T, ang, prm)
   H(θ, ϕ) = Heff(θ, ϕ, ang, prm)
   I(θ, ϕ) = sx(θ, ϕ)*(1/𝒵)*exp(-(1/T)*H(θ, ϕ))*sin(θ)
-  return dblquadgk(I, [0.0, 0.0], [π, 2π])
+  return dblquadgk(I, [0.0, 0.0], [π, 2π], atol=1e-6)
 end
 
 function magy(T, ang::CouplingAngles, prm::Lorentzian)
   𝒵 = 𝒵s(T, ang, prm)
   H(θ, ϕ) = Heff(θ, ϕ, ang, prm)
   I(θ, ϕ) = sy(θ, ϕ)*(1/𝒵)*exp(-(1/T)*H(θ, ϕ))*sin(θ)
-  return dblquadgk(I, [0.0, 0.0], [π, 2π])
+  return dblquadgk(I, [0.0, 0.0], [π, 2π],  atol=1e-6)
 end
 
 function magz(T, ang::CouplingAngles, prm::Lorentzian)
