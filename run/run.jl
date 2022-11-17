98c38abda474a27cc82da6fdf0bfab6f287966ee
Hogg <crh222@exeter.ac.uk>
2022-11-17 16:43:49 +0000

    New parameter sets added to match TEMPO.

--- run/run.jl
+++ run/run.jl
@@ -4,14 +4,23 @@ using DataFrames
 include("../lib/MFGS.jl")
 using .MFGS
 
-# prma = 2.0, 0.001, 10.0
-# prmb = 2.0, 0.001, 1.0
+### Low Gamma ###
+#prma = 2.0, 0.001, 10.0
+#prmb = 2.0, 0.001, 1.0
 
-# prm = LorPrm1D(2.0, 0.001, 10.0) 
-# prm = LorPrm2D(2.0, 0.001, 10.0, 2.0, 0.001, 10.0) 
-prm = LorPrm3D(2.0, 0.001, 10.0, 2.0, 0.001, 10.0, 2.0, 0.001, 10.0) # Lorentzian parameters
-# ang =  CouplAng1D(0.0, 0.0) # Coupling angles
-# ang =  CouplAng2D(π/2, 0.0, 0.0, 0.0) # Coupling angles
+### Weak ###
+#prmc = 2.0, 0.001, 0.1
+
+### High Gamma ###
+#prmd = 2.0, 0.6, 10.0
+#prme = 2.0, 0.6, 1.0
+
+### Parameters ###
+# prm = LorPrm1D(2., 0.6, 1.) 
+# prm = LorPrm2D(2., 0.001, 10., 2., 0.001, 10.) 
+prm = LorPrm3D(2., 0.6, 1., 2., 0.6, 1., 2., 0.6, 1.) # Lorentzian parameters
+# ang =  CouplAng1D(π/4, 0.0) # Coupling angles
+# ang =  CouplAng2D(π/2, 0.0, π/2, π/2) # Coupling angles
 ang =  CouplAng3D(π/2, 0.0, π/2, π/2, 0.0, 0.0) # Coupling angles
 
 ### Temperature Range ###
@@ -24,4 +33,4 @@ magz_list = [magz(i, ang, prm) for i in T]
 
 ### Store Values ###
 df = DataFrame(hcat(T, magx_list, magy_list, magz_list), :auto)
-CSV.write("C://Users//crh222//Data//3DProj//Classical//MFGS_3D_prma.csv",  df, header = ["T", "sx", "sy", "sz"])
+CSV.write("C://Users//crh222//ClassicalSpinMFGS//paper_data//cl_MFGS_3D_prme.csv",  df, header = ["T", "sx", "sy", "sz"])
