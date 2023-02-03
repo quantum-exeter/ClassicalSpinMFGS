3f9e3d0c9ebb14e264d77b32e66cc71494628e68
Charlie Hogg <charlotte.hogg@me.com>
2023-02-03 20:16:05 +0000

    Gibbs partition function added.

--- lib/partitionFunctions.jl
+++ lib/partitionFunctions.jl
@@ -2,6 +2,11 @@
 #### paritionFunctions.jl ####
 ##############################
 
+function 𝒵Gs(T)
+    I(θ, ϕ) = sin(θ)*exp(-BigFloat((1/T)*HS(θ)))
+    return dblquadgk(I, [0.0, 0.0], [π, 2π])
+end
+
 function 𝒵s(T, ang::CouplingAngles, prm::Lorentzian)
     I(θ, ϕ) = sin(θ)*exp(-BigFloat((1/T)*Heff(θ, ϕ, ang, prm)))
     return dblquadgk(I, [0.0, 0.0], [π, 2π])
