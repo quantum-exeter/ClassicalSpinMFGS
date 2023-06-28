19ffddb50abd1d526848ca9543953c1d746d1c18
Hogg <charlotte.hogg@me.com>
2023-06-28 16:17:27 +0100

    File neatened up.

--- run/run.jl
+++ run/run.jl
@@ -6,30 +6,39 @@ using ProgressMeter
 include("../lib/MFGS.jl")
 using .MFGS
 
-### Low Gamma ###
-#prma = 2.0, 0.001, 10.0
-#prmb = 2.0, 0.001, 1.0
-
-### Weak ###
-#prmc = 2.0, 0.001, 0.1
-
-### High Gamma ###
-#prmd = 2.0, 0.6, 10.0
-#prme = 2.0, 0.6, 1.0
-
 ### Parameters ###
-prm = LorPrm1D(2., 0.6, 10.) 
-# prm = LorPrm2D(2., 0.001, 10., 2., 0.001, 10.) 
-# prm = LorPrm3D(2., 0.6, 10., 2., 0.6, 10., 2., 0.6, 10.) # Lorentzian parameters
-ang =  CouplAng1D(atan(sqrt(2)), π/4) # Coupling angles
-# ang =  CouplAng2D(π/2, 0.0, π/2, π/2) # Coupling angles
-# ang =  CouplAng3D(π/2, 0.0, π/2, π/2, 0.0, 0.0) # Coupling angles
 
-### Temperature Range ###
+## Lorentzian spectral density ##
+# {ω_0, Γ, α}
+#prma = 2., 0.001, 10.
+#prmb = 2., 0.001, 1. 
+#prmc = 2., 0.001, 0.1 
+
+#prmd = 2., 0.6, 10.
+#prme = 2., 0.6, 1.
+#prmf = 2., 0.6, 0.1
+
+#prmg = 2., 0.6, 20.
+#prmh = 2., 0.6, 60.
+#prmi = 2., 0.6, 80.
+#prmj = 2., 0.6, 500.
+#prmk = 2., 0.6, 1000.
+
+prm = LorPrm1D(2., 0.6, 10.) 
+# prm = LorPrm2D(2., 0.6, 10., 2., 0.6, 10.) 
+# prm = LorPrm3D(2., 0.6, 10., 2., 0.6, 10., 2., 0.6, 10.)
+
+## Coupling angles ##
+ang =  CouplAng1D(atan(sqrt(2)), π/4)
+# ang =  CouplAng2D(π/2, 0.0, π/2, π/2)
+# ang =  CouplAng3D(π/2, 0.0, π/2, π/2, 0.0, 0.0)
+
+### Temperature range ###
 T = exp10.(range(-3, 2, length=200));
 
-### Gibbs Magnetisations ###
+### Gibbs state spin values ###
 
+## Initialise lists ##
 sxGS_list = zeros(length(T))
 syGS_list = zeros(length(T))
 szGS_list = zeros(length(T))
@@ -40,8 +49,9 @@ szGS_list = zeros(length(T))
     szGS_list[i] = magGz(big(T[i]))
 end
 
-### MFGS Magnetisations ###
+### MF state spin values ###
 
+## Initialise lists ##
 sxMFGS_list = zeros(length(T))
 syMFGS_list = zeros(length(T))
 szMFGS_list = zeros(length(T))
@@ -52,9 +62,10 @@ szMFGS_list = zeros(length(T))
     szMFGS_list[i] = magz(big(T[i]), ang, prm)
 end
 
-### Store Values ###
+### Store values ###
 dfGibbs = DataFrame(hcat(T, sxGS_list, syGS_list, szGS_list), :auto)
 df = DataFrame(hcat(T, sxMFGS_list, syMFGS_list, szMFGS_list), :auto)
 
-CSV.write("./paper_data/cl_Gibbs.csv",  dfGibbs, header = ["T", "sxGS", "syGS", "szGS"])
-CSV.write("./paper_data/cl_MFGS_1D_prmd.csv",  df, header = ["T", "sxMFGS", "syMFGS", "szMFGS"])
+### Export ###
+CSV.write("./paper_data/filename.csv",  dfGibbs, header = ["T", "sxGS", "syGS", "szGS"])
+CSV.write("./paper_data/filename.csv",  df, header = ["T", "sxMFGS", "syMFGS", "szMFGS"])
