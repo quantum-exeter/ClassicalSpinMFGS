47cd7d7ba132913e9626c3716766c01e66e2141d
Hogg <charlotte.hogg@me.com>
2023-06-28 16:10:13 +0100

    Export names changed and file neatened up.

--- lib/MFGS.jl
+++ lib/MFGS.jl
@@ -1,20 +1,23 @@
-#################
-#### MFGS.jl ####
-#################
-
 module MFGS
 
+    ### Import packages ###
     using LinearAlgebra
     using Kronecker
     using QuadGK
 
-    export LorPrm1D, LorPrm2D, LorPrm3D, CouplAng1D, CouplAng2D, CouplAng3D, magGx, magGy, magGz, magx, magy, magz
-
+     ### Inclusions ###
     include("variables.jl")
     include("spinComponents.jl")
     include("hamiltonians.jl")
     include("integration.jl")
     include("partitionFunctions.jl")
     include("magnetisation.jl")
+
+    ####################################
+    ####################################
+    ####################################
+
+    ### Exports ###
+    export LorPrm1D, LorPrm2D, LorPrm3D, CouplAng1D, CouplAng2D, CouplAng3D, sxGibbs, syGibbs, szGibbs, sx, sy, sz
 
 end
