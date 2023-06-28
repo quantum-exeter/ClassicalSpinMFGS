0797f9d704d2ae5d1f87026b6a3408bf6145de1a
Hogg <charlotte.hogg@me.com>
2023-06-28 16:11:19 +0100

    Comments added.

--- lib/partitionFunctions.jl
+++ lib/partitionFunctions.jl
@@ -2,11 +2,13 @@
 #### paritionFunctions.jl ####
 ##############################
 
+### Partition function for Gibbs state ###
 function 𝒵Gs(T)
     I(θ, ϕ) = sin(θ)*exp(-BigFloat((1/T)*HS(θ)))
     return dblquadgk(I, [0.0, 0.0], [π, 2π])
 end
 
+### Partition function for MF state ###
 function 𝒵s(T, ang::CouplingAngles, prm::Lorentzian)
     I(θ, ϕ) = sin(θ)*exp(-BigFloat((1/T)*Heff(θ, ϕ, ang, prm)))
     return dblquadgk(I, [0.0, 0.0], [π, 2π])
