cf0c1b2621ff80c52329dfa1199609d7d244528a
Hogg <charlotte.hogg@me.com>
2023-06-28 15:52:27 +0100

    Comments added.

--- lib/integration.jl
+++ lib/integration.jl
@@ -2,6 +2,7 @@
 #### integration.jl ####
 ########################
 
+## Double integration ##
 function dblquadgk(f, a::AbstractArray{T}, b::AbstractArray{T}; rtol=sqrt(eps(T)), atol=zero(T), maxevals=10^7, order=7) where T<:AbstractFloat
     J(x) = quadgk(y -> f(x,y), a[2], b[2], atol=atol, maxevals=maxevals, order=order)[1]
     K = quadgk(x -> J(x), a[1], b[1], atol=atol, maxevals=maxevals, order=order)[1]
